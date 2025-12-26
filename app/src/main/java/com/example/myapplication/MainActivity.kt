aa87d9c817034ebadcbaf77612500464b2857a6c
maksympash <maksympash@gmail.com>
2025-12-26 14:16:59 +0200

    Creating columns, rows and added ElevatedButton

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -28,30 +33,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(modifier: Modifier = Modifier) {
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Android"),
+    ) {
     Surface(
-        color = MaterialTheme.colorScheme.primary,
+        color = MaterialTheme.colorScheme.background,
         modifier = modifier
     ) {
-        Greeting("Android")
+        Column (modifier = modifier.padding(vertical = 4.dp)) {
+            for (name in names) {
+                Greeting(name)
+            }
+        }
+
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface (
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp),
         color = MaterialTheme.colorScheme.primary
     ) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-                .padding(24.dp)
-        )
-    }
+        Row ( modifier = modifier
+            .padding(24.dp)
+        ){
+            Column(
+                modifier = modifier
+                    .weight(1f)
+            ) {
+                Text(text = "Hello ")
+                Text(text = name)
+            }
 
+            ElevatedButton(
+                onClick = {}
+            ) {
+                Text("Show more")
+            }
+        }
+
+    }
 }
 
-@Preview(showBackground = true, name = "Text preview")
+@Preview(showBackground = true, name = "Text preview", widthDp = 320)
 @Composable
 fun GreetingPreview() {
     BasicsCodelabTheme {
