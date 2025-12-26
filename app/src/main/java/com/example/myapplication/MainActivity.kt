ece51275f84bbfc5141e13e4f1316eec96ec9216
maksympash <maksympash@gmail.com>
2025-12-26 13:38:56 +0200

    Tweaking the UI(Modifier and MaterialTheme)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +34,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface (
+        color = MaterialTheme.colorScheme.primary
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+                .padding(24.dp)
+        )
+    }
+
 }
 
 @Preview(showBackground = true, name = "Text preview")
